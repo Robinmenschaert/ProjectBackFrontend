b42670e9454a7b9e530005ba22e5efb5df9e854b
FUNExtreme <robin.maenhaut.coder@gmail.com>
2017-01-05 20:04:22 +0100

    Draw targets + hit detection

--- frontend/src/app/directives/game-canvas.directive.ts
+++ frontend/src/app/directives/game-canvas.directive.ts
@@ -4,12 +4,19 @@ import { Directive, ElementRef } from '@angular/core';
   selector: '[appGameCanvas]'
 })
 export class GameCanvasDirective {
+  // Constants
+  projectileRadius: number = 5;
+  selfTankSize: Size = new Size(45, 50);
+
+  //
   canvasSize: Size;
   canvasContext: CanvasRenderingContext2D;
 
-  selfTankSize: Size;
+  
   selfTankImage:any;
   selfProjectiles: Array<Projectile>;
+
+  targets: Target[];
 
   self: Self;
   isMoving: boolean;
@@ -19,9 +26,6 @@ export class GameCanvasDirective {
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
 
-    this.selfTankSize = new Size();
-    this.selfTankSize.width = 45;
-    this.selfTankSize.height = 50;
     this.selfTankImage = new Image();
     this.selfTankImage.src = "/assets/selfTank2.png";
     this.selfProjectiles = new Array<Projectile>();
@@ -41,7 +45,7 @@ export class GameCanvasDirective {
 
     canvas.addEventListener("mousemove", this.onMouseMove);
 
-    this.animate();
+    this.init();
   }
 
   onKeyDown = (event: KeyboardEvent) => {
@@ -69,6 +73,19 @@ export class GameCanvasDirective {
     this.self.mousePosition.y = event.clientY;
   }
 
+  init = () => {
+    /* TEMP CALCULATE TARGETS, WILL MOVE TO SERVER EVENTUALLY */
+    const targetCount = 5;
+    this.targets = new Array<Target>(targetCount);
+    for(let x = 0; x < targetCount; x ++) {
+      this.targets[x] = new Target();
+      this.targets[x].position = new Position(Math.random() * this.canvasSize.width, Math.random() * this.canvasSize.height);
+      this.targets[x].character = "A";
+    }
+
+    this.animate();
+  }
+
   animate = () => {
     requestAnimationFrame(this.animate);
     this.update();
@@ -78,6 +95,8 @@ export class GameCanvasDirective {
   update = () => {
     this.calculateSelfPosition();
     this.calculateSelfProjectilesPosition();
+
+    this.performSelfHitDetection();
   }
 
   calculateSelfPosition = () => {
@@ -96,22 +115,46 @@ export class GameCanvasDirective {
 
       this.self.position.x += Math.sin(this.self.viewAngle) * speed;
       this.self.position.y -= Math.cos(this.self.viewAngle) * speed;
-    }
+    } 
   }
 
   calculateSelfProjectilesPosition = () => {
     const speed = 7;
 
     for(let projectile of this.selfProjectiles) {
-      projectile.position.x += Math.sin(projectile.angle) * speed;
-      projectile.position.y -= Math.cos(projectile.angle) * speed;
+      if(!projectile.hasHit) {
+        projectile.position.x += Math.sin(projectile.angle) * speed;
+        projectile.position.y -= Math.cos(projectile.angle) * speed;
+      }
+    }
+  }
+
+  performSelfHitDetection = () => {
+    for(let projectileIndex = this.selfProjectiles.length - 1; projectileIndex >= 0; projectileIndex --) {
+      for(let targetIndex = this.targets.length - 1; targetIndex >= 0; targetIndex --) {
+        let projectile = this.selfProjectiles[projectileIndex];
+        let target = this.targets[targetIndex];
+
+        let pP = projectile.position;
+        let tP = target.position;
+        
+        if (pP.x < tP.x + this.selfTankSize.width 
+            && pP.x + this.projectileRadius > tP.x 
+            && pP.y < tP.y + this.selfTankSize.height 
+            && this.projectileRadius + pP.y > tP.y) {
+
+            this.selfProjectiles.splice(projectileIndex, 1);
+            this.targets.splice(targetIndex, 1);
+            break;
+        }
+      } 
     }
   }
 
   draw = () => {
     this.canvasContext.clearRect(0,0, this.canvasSize.width, this.canvasSize.height);
     this.drawSelf();
-    this.drawEnemies();
+    this.drawTargets();
     this.drawTanks();
     this.drawSelfProjectiles();
   }
@@ -121,22 +164,44 @@ export class GameCanvasDirective {
     this.canvasContext.translate(this.self.position.x, this.self.position.y);
     this.canvasContext.rotate(this.self.viewAngle);
     this.canvasContext.drawImage(this.selfTankImage, -(this.selfTankSize.width/2), -(this.selfTankSize.height/2), this.selfTankSize.width, this.selfTankSize.height);
-    this.canvasContext.restore(); 
+    this.canvasContext.restore();  
   }
-  drawEnemies = () => {
+  drawTargets = () => {
+    this.canvasContext.save();
+    this.canvasContext.font = "25px serif";
+
+    for(let target of this.targets) {
+      this.canvasContext.fillText(target.character, target.position.x, target.position.y + 35);
+
+      // debug
+      this.canvasContext.strokeRect(target.position.x - 7, target.position.y + 7, 35, 35);
+    }    
+    this.canvasContext.restore();   
   }
   drawTanks = () => {
+    this.canvasContext.save();
+
+    this.canvasContext.restore(); 
   }
   drawSelfProjectiles = () => {
     for(let projectile of this.selfProjectiles) {
+      this.canvasContext.save();
       this.canvasContext.beginPath();
-      this.canvasContext.arc(projectile.position.x, projectile.position.y, 5, 0, 2 * Math.PI, false);
-      this.canvasContext.fillStyle = 'green';
+      this.canvasContext.arc(projectile.position.x, projectile.position.y, this.projectileRadius, 0, 2 * Math.PI, false);
+
+      if(projectile.hasHit) {
+        this.canvasContext.fillStyle = 'red';
+      }
+      else {
+        this.canvasContext.fillStyle = 'green';
+      }
+      
       this.canvasContext.fill();
       this.canvasContext.lineWidth = 5;
       this.canvasContext.strokeStyle = '#003300';
       this.canvasContext.stroke();
-    }
+      this.canvasContext.restore(); 
+    } 
   }
 }
 
@@ -153,6 +218,11 @@ export class Position {
 export class Size {
   width: number;
   height: number;
+
+  constructor(width?: number, height?: number) {
+    this.width = width;
+    this.height = height;   
+  }
 }
 
 export class Self {
@@ -164,9 +234,16 @@ export class Self {
 export class Projectile {
   position: Position;
   angle: number;
+  hasHit: boolean;
 
   constructor(initialX?: number, initialY?: number, angle?: number) {
     this.position = new Position(initialX, initialY);
     this.angle = angle;
-  }
+  } 
+}
+
+export class Target {
+  position: Position;
+  character: string;
+  isHit: boolean;
 }
